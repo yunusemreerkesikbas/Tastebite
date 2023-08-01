2ceb117e6dd47ea7ff0b9cf16db2c881eaf97dce
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-08-01 17:40:11 +0300

    initial package files and file structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { MobileHeaderComponent } from './components/layouts/mobile-header/mobile-header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { SearchComponent } from './pages/search/search.component';
+import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    MobileHeaderComponent,
+    FooterComponent,
+    CategoryComponent,
+    SearchComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
